7a47659525c28817f81d0d2e1cfa051e9a427a7e
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-21 00:59:06 +0300

    created a function to fetch films and display  available tickets

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,26 @@ const showtime = document.getElementById('showtime');
 const availableTickets = document.getElementById('available-tickets');
 const description = document.getElementById('description');
 const buyTicket = document.getElementById('buy-ticket');
+
+let currentFilm = null;
+
+function fetchFilms(){
+    fetch('http:localhoost:3000/films')
+    .then(res=> res.json())
+    .then(films => {
+        filmList.innerHTML = '';
+
+        films.forEach(film => {
+            const li = document.createElement('li');
+            li.textContent = film.title;
+            li.classList.add('filt-item');
+            if(film.capacity - film.tickets.sold <= 0){
+                li.classList.add('sold-out');
+                li.textContent += "(Sold-Out)";
+            }
+            li.dataset.id = film.id;
+            filmList.appendChild(li);
+        });
+        showFilmDetails(films[0]);
+    });
+}
